5b087b45034539a6f23aea81566f0b57b0636fbf
Niketan Gulekar <nikegulekar@gmail.com>
2020-07-03 19:43:45 +0530

    Update config.js

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAc6rI6OoNtiiMeF2SuUK9iqZIXyDs1eos",
-    authDomain: "todoapp-7ee2e.firebaseapp.com",
-    databaseURL: "https://todoapp-7ee2e.firebaseio.com",
-    projectId: "todoapp-7ee2e",
-    storageBucket: "todoapp-7ee2e.appspot.com",
-    messagingSenderId: "154178414758",
-    appId: "1:154178414758:web:8a5bfddb8b2de9a9cd2c67",
-    measurementId: "G-Z2TC0FPETL"
+    apiKey: "XXXXX",
+    authDomain: "XXXXX",
+    databaseURL: "XXXXX",
+    projectId: "XXXXX",
+    storageBucket: "XXXXX",
+    messagingSenderId: "XXXXX",
+    appId: "XXXXX",
+    measurementId: "XXXXX"
 };
